6bb8c3c2234f563ce09cb6c3c286b6d04bfa47de
Oscar Martinez <masquesig@gmail.com>
2019-04-07 21:32:27 +0200

    int

---
+++ main.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+import gvsig
+
+from gvsig import getResource
+from gvsig.libs.formpanel import FormPanel
+
+class ToolQuickDrawing(FormPanel):
+  def __init__(self):
+    FormPanel.__init__(self,getResource(__file__,"main.xml"))
+
+def main(*args):
+  p = ToolQuickDrawing()
+  p.showTool("QuikcDrawing")

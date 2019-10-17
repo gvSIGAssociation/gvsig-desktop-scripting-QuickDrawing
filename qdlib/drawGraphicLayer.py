71eafe3be193a76f3ec34a614accb274606ad8a3
Oscar Martinez <masquesig@gmail.com>
2019-10-17 14:34:01 +0200

    Geom del ftype de tipo GEOMETRY

--- qdlib/drawGraphicLayer.py
+++ qdlib/drawGraphicLayer.py
@@ -11,6 +11,7 @@ from org.gvsig.fmap.dal import DALLocator
 from org.gvsig.fmap.mapcontext import MapContextLocator
 
 from org.gvsig.fmap.mapcontext.layers.vectorial import FLyrVect
+from org.gvsig.fmap.geom import Geometry
 
 def createMemoryStore():
   dataManager = DALLocator.getDataManager()
@@ -22,21 +23,36 @@ def createMemoryStore():
   eft.append("ID", 'STRING', 38)
   eft.get("ID").setIsPrimaryKey(True)
   eft.append("COUTLINE", "INTEGER", 9)
+  eft.get("COUTLINE").setDefaultValue(16724787)
+  
   eft.append("CFILL", "INTEGER", 9)
+  eft.get("CFILL").setDefaultValue(-65536)
+  
   eft.append("CSIZE", "INTEGER", 9)
+  eft.get("CSIZE").setDefaultValue(2)
+  
   eft.append("CROTATION", "INTEGER", 9)
+  eft.get("CROTATION").setDefaultValue(0)
   
   eft.append("LTEXT", 'STRING', 150)
+  eft.get("LTEXT").setDefaultValue("")
   eft.append("LCOLOR", 'INTEGER', 9)
+  eft.get("LCOLOR").setDefaultValue(1258291200)
   eft.append("LROTATION", 'INTEGER', 9)
+  eft.get("LROTATION").setDefaultValue(0)
   eft.append("LFONT", 'STRING', 150)
+  eft.get("LFONT").setDefaultValue("Serif")
   eft.append("LFONTS", 'INTEGER', 9)
+  eft.get("LFONTS").setDefaultValue(0)
   eft.append("LHEIGHT", 'INTEGER', 5)
+  eft.get("LHEIGHT").setDefaultValue(10)
   eft.append("LUNIT", 'INTEGER', 2)
+  eft.get("LUNIT").setDefaultValue(1)
   eft.append("LREF", 'INTEGER', 2)
+  eft.get("LREF").setDefaultValue(0)
   
   eft.append("GEOMETRY", "GEOMETRY")
-  eft.get("GEOMETRY").setGeometryType(geom.NULL, geom.UNKNOWN)
+  eft.get("GEOMETRY").setGeometryType(Geometry.TYPES.GEOMETRY, Geometry.SUBTYPES.GEOM2D)
   store.edit()
   store.update(eft)
   store.finishEditing()  
